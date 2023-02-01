be28dde4e517df71cd449f04e5aac80ba07310cd
Malte Hückstädt <47598771+MalteHueckstaedt@users.noreply.github.com>
2023-02-01 11:11:47 +0100

    upd

--- users-aad9b4.php
+++
@@ -1,3 +0,0 @@
-<?php
-    $users = array('mh' => array('id' => 1, 'name' => 'Malte', 'surname' => 'Hückstädt', 'pwd' => 'upB2TiSqPgPTMTgXBl2N94kJlD0crnwe', 'email' => 'deaddatascientists@gmail.com', 'info' => '', 'groups' => array('0')));
-    $salt = '$2y$10$H7yNiatolrlp5aoPkptpa1$';
